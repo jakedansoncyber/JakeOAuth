b40b2e967fb2778fe70f0d91c260add8d933be6f
Jake Danson <96601747+jakedansoncyber@users.noreply.github.com>
2024-10-31 00:02:14 -0700

    authorization code grant progress...not quite done yet, lots of clean… (#4)
    * authorization code grant progress...not quite done yet, lots of clean up needed
    
    * fix up tests

--- auth/jwt.go
+++ auth/jwt.go
@@ -6,28 +6,38 @@ import (
 	"os"
 )
 
+var (
+	PathToPrivateKey = "keys/private.pem"
+	PathToPublicKey  = "keys/public.pub"
+)
+
 func CreateJWT() (token *jwt.Token, err error) {
 	//TODO create JWT based off of client id
 
 	token = jwt.New(jwt.SigningMethodRS256)
 
-	b, err := os.ReadFile("../keys/private.pem")
+	b, err := os.ReadFile(PathToPrivateKey)
 
 	if err != nil {
 		fmt.Printf("failed to read private key file error: %s\n", err)
+		panic(err)
 	}
 
 	key, err := jwt.ParseRSAPrivateKeyFromPEM(b)
 
 	if err != nil {
 		fmt.Printf("failed to parse private key: %s\n", err)
+		panic(err)
 	}
 
 	tokenString, err := token.SignedString(key)
 
 	if err != nil {
 		fmt.Printf("failed to sign string %s\n", err)
+		panic(err)
 	}
+
+	token.Raw = tokenString
 
 	fmt.Println("token string: " + tokenString)
 
@@ -35,7 +45,7 @@ func CreateJWT() (token *jwt.Token, err error) {
 }
 
 func ValidateToken(tokenString string) (bool, error) {
-	b, err := os.ReadFile("../keys/public.pub")
+	b, err := os.ReadFile(PathToPublicKey)
 
 	if err != nil {
 		fmt.Printf("failed to read public key file %s\n", err)
--- auth/memory_test.go
+++ auth/memory_test.go
@@ -1,6 +1,8 @@
 package auth
 
 import (
+	"crypto/sha256"
+	"encoding/base64"
 	"fmt"
 	"github.com/stretchr/testify/assert"
 	"testing"
@@ -8,19 +10,21 @@ import (
 )
 
 func TestNewAuthorizationCode(t *testing.T) {
-	ac := NewAuthorizationCode()
+	code, _ := generateASCII(45)
+	ac := NewAuthorizationCode(code, "plain", "")
 	fmt.Println(ac)
 	assert.NotNil(t, ac.Code)
 	assert.NotNil(t, ac.Pkce)
-	assert.Greater(t, ac.Exp, time.Now().Add(CodeExpiration-(1*time.Minute)))
+	assert.Greater(t, time.Unix(ac.Exp, 0), time.Now().Add(CodeExpiration-(1*time.Minute)))
 }
 
 func TestNewAuthCodeStore(t *testing.T) {
+	code, _ := generateASCII(45)
 	CodeExpiration = 1 * time.Second // this is a global var set in memory.go
 	acs := NewAuthCodeStore()
 	go acs.ListenExpiration()
 	for i := 0; i <= 200; i++ {
-		ac := NewAuthorizationCode()
+		ac := NewAuthorizationCode(code, "S256", "")
 		acs.Add(ac)
 	}
 
@@ -30,14 +34,17 @@ func TestNewAuthCodeStore(t *testing.T) {
 }
 
 func TestAuthorizationCodeStore_CheckTokenWithPkce_Valid(t *testing.T) {
+	pkce, _ := generateASCII(45)
+	shaBytes := sha256.Sum256([]byte(pkce))
+	codeChallenge := base64.RawURLEncoding.EncodeToString(shaBytes[:])
 	CodeExpiration = 5 * time.Minute
 	acs := NewAuthCodeStore()
 	go acs.ListenExpiration()
 
-	ac := NewAuthorizationCode()
+	ac := NewAuthorizationCode(codeChallenge, "S256", "")
 	acs.Add(ac)
 
-	isValid, err := acs.CheckTokenWithPkce(ac.Code, ac.Pkce)
+	isValid, err := acs.CheckTokenWithPkce(ac.Code, pkce)
 
 	assert.Nil(t, err)
 	assert.True(t, isValid)
@@ -45,14 +52,16 @@ func TestAuthorizationCodeStore_CheckTokenWithPkce_Valid(t *testing.T) {
 
 func TestAuthorizationCodeStore_CheckTokenWithPkce_Expired(t *testing.T) {
 	CodeExpiration = 1 * time.Microsecond // this is a global var set in memory.go
-
+	pkce, _ := generateASCII(45)
+	shaBytes := sha256.Sum256([]byte(pkce))
+	codeChallenge := base64.RawURLEncoding.EncodeToString(shaBytes[:])
 	acs := NewAuthCodeStore()
 	go acs.ListenExpiration()
 
-	ac := NewAuthorizationCode()
+	ac := NewAuthorizationCode(codeChallenge, "S256", "")
 	acs.Add(ac)
 	time.Sleep(20 * time.Microsecond)
-	isValid, err := acs.CheckTokenWithPkce(ac.Code, ac.Pkce)
+	isValid, err := acs.CheckTokenWithPkce(ac.Code, pkce)
 
 	assert.Error(t, err)
 	assert.False(t, isValid)
@@ -60,12 +69,15 @@ func TestAuthorizationCodeStore_CheckTokenWithPkce_Expired(t *testing.T) {
 
 func TestAuthorizationCodeStore_CheckTokenWithPkce_NotInCodeStore(t *testing.T) {
 	CodeExpiration = 5 * time.Minute
+	pkce, _ := generateASCII(45)
+	shaBytes := sha256.Sum256([]byte(pkce))
+	codeChallenge := base64.RawURLEncoding.EncodeToString(shaBytes[:])
 	acs := NewAuthCodeStore()
 	go acs.ListenExpiration()
 
-	ac := NewAuthorizationCode()
+	ac := NewAuthorizationCode(codeChallenge, "S256", "")
 	time.Sleep(20 * time.Microsecond)
-	isValid, err := acs.CheckTokenWithPkce(ac.Code, ac.Pkce)
+	isValid, err := acs.CheckTokenWithPkce(ac.Code, pkce)
 
 	if assert.Error(t, err) {
 		assert.Equal(t, "token not found in code store", err.Error())
@@ -75,15 +87,21 @@ func TestAuthorizationCodeStore_CheckTokenWithPkce_NotInCodeStore(t *testing.T) 
 
 func TestAuthorizationCodeStore_CheckTokenWithPkce_InvalidPkce(t *testing.T) {
 	CodeExpiration = 5 * time.Minute
+	pkce, _ := generateASCII(45)
+	shaBytes := sha256.Sum256([]byte(pkce))
+	codeChallenge := base64.RawURLEncoding.EncodeToString(shaBytes[:])
 	acs := NewAuthCodeStore()
 	go acs.ListenExpiration()
 
-	ac := NewAuthorizationCode()
+	ac := NewAuthorizationCode(codeChallenge, "S256", "")
 	acs.Add(ac)
 
-	acBadCode := NewAuthorizationCode()
+	pkce2, _ := generateASCII(45)
+	shaBytes2 := sha256.Sum256([]byte(pkce2))
+	codeChallenge2 := base64.RawURLEncoding.EncodeToString(shaBytes2[:])
+	acBadCode := NewAuthorizationCode(codeChallenge2, "S256", "")
 	acBadCode.Code = ac.Code
-	isValid, err := acs.CheckTokenWithPkce(acBadCode.Code, acBadCode.Pkce)
+	isValid, err := acs.CheckTokenWithPkce(acBadCode.Code, pkce2)
 
 	if assert.Error(t, err) {
 		assert.Equal(t, "pkce code was not the same", err.Error())
